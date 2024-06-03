e28683b75e0d6d0eab0edfa01ede0f33664a7705
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-03 20:49:55 +0800

    feat: add user controller

---
+++ controllers/users.js
@@ -0,0 +1,20 @@
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  try {
+
+    const newUser = await User.create({
+      first_name: request.body.first_name, 
+      last_name: request.body.last_name, 
+      password:request.body.password
+    })
+  
+    return response.status(201).json(newUser)
+
+  } catch(error) {
+    return response.status(400).json({'error': error.errors[0].message})
+  }
+})
+
+module.exports = usersRouter
