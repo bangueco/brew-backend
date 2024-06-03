7e821027beaddf627e412b6ac1532cf761d6e882
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-03 19:57:25 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const { DataTypes} = require('sequelize')
+const sequelize = require('../database/db')
+
+const User = sequelize.define(
+  'User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }
+)
+
+module.exports = User
