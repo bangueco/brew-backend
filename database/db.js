fabfb2e52715ef7702722995eb406e5eb0e5290c
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-03 19:56:10 +0800

    refactor: change sqlite3 connection to sequelize

--- database/db.js
+++ database/db.js
@@ -1,23 +1,8 @@
-const path = require('path')
-const logger = require('../utils/logger')
-const sqlite3 = require('sqlite3').verbose()
+const { Sequelize } = require('sequelize')
 
-
-// Connect to database
-const dbPath = path.resolve(__dirname, 'brew.db')
-const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (error) => {
-  if (error) logger.error(error.message)
-
-  logger.info('Connected to database')
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '../database/brew.db'
 });
 
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS users(
-    id INTEGER PRIMARY KEY,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    password TEXT NOT NULL
-  )`)
-})
-
-module.exports = db
+module.exports = sequelize
