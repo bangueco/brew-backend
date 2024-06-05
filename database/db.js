b4c92df77ae8d12f0a1f0643c6aef69e60baeee7
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-05 21:10:04 +0800

    feat: add database for testing and production

--- database/db.js
+++ database/db.js
@@ -2,9 +2,13 @@ const path = require('path')
 const { Sequelize } = require('sequelize')
 const logger = require('../utils/logger')
 
+const config = require('../utils/config')
+
+const databasePath = config.DATABASE
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: path.resolve(__dirname, './brew.db')
+  storage: path.resolve(__dirname, databasePath)
 });
 
 const syncTable = async () => {
--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
+const DATABASE = process.env.NODE_ENV === 'test' ? process.env.TEST_DB : process.env.PROD_DB
 
 module.exports = {
-  PORT
+  PORT,
+  DATABASE
 }
