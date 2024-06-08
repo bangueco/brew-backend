339ef5b4996290f969453c94dbb9c9ba8f02ffdc
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-08 20:51:32 +0800

    feat: add drinks creation

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -3,7 +3,25 @@ const Drink = require('../models/drink')
 
 productsRouter.get('/', async (request, response) => {
   const products = await Drink.findAll()
-  return response.status(200).json({products})
+  return response.status(200).json(products)
+})
+
+productsRouter.post('/', async (request, response, next) => {
+
+  try {
+    
+    let {name, price} = request.body
+  
+    const newDrink = await Drink.create({
+      name, price
+    })
+  
+    return response.status(201).json({newDrink})
+
+  } catch(error) {
+    next(error)
+  }
+
 })
 
 module.exports = productsRouter
