c86be652e06a12eaa6601edb4bf0a7668ab482f5
bangueco <121280104+bangueco@users.noreply.github.com>
2024-06-19 14:36:43 +0800

    feat: add router to get a specific product

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -6,6 +6,11 @@ productsRouter.get('/', async (request, response) => {
   return response.status(200).json(products)
 })
 
+productsRouter.get('/:id', async (request, response) => {
+  const product = await Drink.findByPk(request.params.id)
+  return response.status(200).json(product)
+})
+
 productsRouter.post('/', async (request, response, next) => {
 
   try {
